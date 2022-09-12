541bc0d9a4b30bdc8f831d2fbe3dfbd101748aa1
mannacoder <vaibhavv.singh11@gmail.com>
2022-09-13 01:59:47 +0530

    final

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -17,6 +17,16 @@ const About = () => {
           elit vitae, accumsan arcu. Integer ac felis et quam tristique blandit.
         </p>
         <p class='lead'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+          vestibulum eleifend leo at rutrum. Suspendisse hendrerit libero ac
+          nisi venenatis faucibus. Curabitur blandit nec ante et accumsan.
+          Nullam eu mauris maximus, pulvinar ipsum id, tempor enim. Vivamus eget
+          dolor molestie, lobortis nunc at, iaculis odio. Vestibulum est orci,
+          pretium id arcu sit amet, mattis hendrerit dui. Praesent eget ligula
+          ac odio auctor interdum eu eget nisl. Proin in ligula mattis, commodo
+          elit vitae, accumsan arcu. Integer ac felis et quam tristique blandit.
+        </p>
+        <p class='lead'>
           <a href='#' class='btn btn-lg btn-secondary'>
             Learn more
           </a>
